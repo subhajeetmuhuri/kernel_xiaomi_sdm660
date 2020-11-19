8ebf1d5b5b68e2077760c0e45a943eabaa86c379
Danny Lin <danny@kdrag0n.dev>
2020-11-18 17:49:13 -0800

    power: Silence debug logging if logbuffer is disabled
    If logbuffer is disabled, we want to silence logging, not dump it to
    kmsg.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -40,14 +40,7 @@ extern int hwc_check_global;
 		__func__, ##__VA_ARGS__)	\
 
 #define smblib_dbg(chg, reason, fmt, ...)			\
-	do {							\
-		if (*chg->debug_mask & (reason))		\
-			pr_info("%s: %s: " fmt, chg->name,	\
-				__func__, ##__VA_ARGS__);	\
-		else						\
-			pr_debug("%s: %s: " fmt, chg->name,	\
-				__func__, ##__VA_ARGS__);	\
-	} while (0)
+	do { } while (0)
 
 #ifdef CONFIG_MACH_MI
 static bool off_charge_flag;
